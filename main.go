5b6ede4f92ffd96e2b77672eeff4da319c774cbc
liuruichao <liuruichao555@126.com>
2017-05-15 16:30:37 +0800

    init project

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "github.com/liuruichao555/beego-demo1/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/liuruichao555/beego-demo1/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
