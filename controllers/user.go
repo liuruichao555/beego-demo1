4e9ce41a6b91ed1c639845f9ab6b63bc70e33014
liuruichao <liuruichao555@126.com>
2017-08-16 15:02:19 +0800

    add json

--- controllers/user.go
+++ controllers/user.go
@@ -1,12 +1,26 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"encoding/json"
+)
 
 type UserController struct {
 	beego.Controller
 }
 
+type User struct {
+	Username string
+	Age      int
+}
+
 func (c *UserController) Get() {
-	c.Data["json"] = "success"
+	user := User{Username: "刘瑞超", Age: 20}
+	jsonStr, err := json.Marshal(user)
+	if err != nil {
+		c.Data["json"] = err.Error()
+	} else {
+		c.Data["json"] = string(jsonStr)
+	}
 	c.ServeJSON(true)
 }
